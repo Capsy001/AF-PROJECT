d6e1582de6372f62ca11c2ec565c66f8bac9d2c0
Capsy <71144025+Capsy001@users.noreply.github.com>
2022-05-25 14:14:36 +0530

    Added create group UI

--- frontend/components/createGroup.jsx
+++ frontend/components/createGroup.jsx
@@ -12,6 +12,11 @@ import {
   Divider,
   createTheme,
   colors,
+  Paper,
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 
 export default class CreateGroup extends Component {
@@ -19,50 +24,17 @@ export default class CreateGroup extends Component {
     super();
 
     this.state = {
-      name: "",
-      price: "",
-      desc: "",
-      uid: "",
+      member1: "",
+      member2: "",
+      member3: "",
+      member4: "",
+      groupid: "",
     };
   }
 
-  
-
-  handleItemName = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handleDesc = (event) => {
-    this.setState({
-      desc: event.target.value,
-    });
-  };
-
-  handleItemPrice = (event) => {
-    this.setState({
-      price: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    const uid = sessionStorage.getItem("loggedUID");
-
-    const item = {
-      name: this.state.name,
-      price: this.state.price,
-      desc: this.state.desc,
-      uid: uid,
-    };
-
-    addItem(item);
-  };
-
-  componentWillMount() {
-    
+  handleMember1 = (event) =>
+  {
+    this.setState({ member1: event.target.value });
   }
 
   handleLogout = (event) => {
@@ -122,27 +94,89 @@ export default class CreateGroup extends Component {
             </Toolbar>
           </AppBar>
 
-          <h2>Add Items</h2>
+          <h2>Create Group</h2>
 
           <hr></hr>
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <label>Item Name</label>
-              <input type="text" onChange={this.handleItemName}></input>
-            </div>
-            <div>
-              <label>Item Price</label>
-              <input type="number" onChange={this.handleItemPrice}></input>
-            </div>
-            <div>
-              <label>Description</label>
-              <input type="text" onChange={this.handleDesc}></input>
-            </div>
+          <Paper
+            sx={{
+              padding: "32px",
+              width: "40%",
+              textAlign: "center",
+              justifyContent: "center",
+              margin: "0 auto",
+            }}
+          >
+            <form onSubmit={this.handleSubmit}>
+              <div>
+                <TextField
+                  label="Member 1"
+                  variant="outlined"
+                  required
+                  type="text"
+                  value={this.state.member1}
+                  onChange={this.handleMember1}
+                />
+              </div>
+              <br></br>
+              <div>
+                <TextField
+                  label="Member 2"
+                  variant="outlined"
+                  required
+                  type="text"
+                  value={this.state.member2}
+                  onChange={this.handleUserNameChange}
+                />
+              </div>
+              <br></br>
+              <div>
+                <TextField
+                  label="Member 3"
+                  variant="outlined"
+                  required
+                  type="text"
+                  value={this.state.member3}
+                  onChange={this.handleEmailChange}
+                />
+              </div>
+              <br></br>
+              <div>
+                <TextField
+                  label="Member 4"
+                  variant="outlined"
+                  required
+                  type="text"
+                  value={this.state.member4}
+                  onChange={this.handlePasswordChange}
+                />
+              </div>
 
-            <button className="buttonMargin" type="submit">
-              +Add Item
-            </button>
-          </form>
+              <br></br>
+              <div>
+                <TextField
+                  label="Group ID"
+                  variant="outlined"
+                  required
+                  type="text"
+                  value={this.state.groupid}
+                  onChange={this.handlePasswordChange}
+                  disabled
+                
+                />
+              </div>
+              <Button
+                sx={{ width: "50%" }}
+                variant="contained"
+                color="success"
+                id="Submit"
+                size="small"
+                className="buttonMargin"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </form>
+          </Paper>
         </div>
       </div>
     );
