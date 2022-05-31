dfe764354f59c1fbe56143767f0457d440813fd1
Capsy <71144025+Capsy001@users.noreply.github.com>
2022-05-31 15:36:35 +0530

    supervisor UI

--- frontend/Staff/SupervisorAccept.jsx
+++ frontend/Staff/SupervisorAccept.jsx
@@ -5,6 +5,7 @@ import { Button, Chip, Divider, Card, CardContent } from "@mui/material";
 import { Button, TextField, Chip, Divider, Typography, CardActions } from "@mui/material";
 import AppBarNav from "../components/AppBarNav";
 import axios from "axios";
+import { getTopics } from "../ApiCalls/topic.apicall";
 
 export default class SupervisorAccept extends Component {
   constructor(props) {
@@ -18,7 +19,14 @@ export default class SupervisorAccept extends Component {
 
 
 
-  componentDidMount(){
+  async componentDidMount(){
+      const data=await getTopics()
+    this.setState({data: data});
+
+    setTimeout(()=>{
+            console.log(data);
+    }, 1000)
+
     
   }
 
@@ -42,19 +50,19 @@ export default class SupervisorAccept extends Component {
             <div style={{justifyContent:'center',display:'flex',marginTop:'20px'}}>
               
               {
-                ([this.state.data]).map(data => 
-                  <Card sx={{ width: 645 }} style={{borderLeft:'3px solid #2e7d32'}}>
+                this.state.data.map(data => 
+                  <Card sx={{ width: '40%', margin: "5px", display: "block" }} style={{borderLeft:'3px solid #2e7d32', display: "block"}}>
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
-                        
+                        {data.topic}
                       </Typography>
                       <Typography variant="body2" color="text.secondary">
-                        
+                        Group: {data.groupid}
                       </Typography>
                     </CardContent>
                     <CardActions sx={{margin: '0 auto', justifyContent:'center'}}>
-                        <Button variant="outlined" size="small">Accept As Supervisor</Button>
-                        <Button variant="outlined" size="small">Accept As Co-Supervisor</Button>
+                        <Button variant="outlined" size="small" topicid={data._id}>Accept As Supervisor</Button>
+                        <Button variant="outlined" size="small" topicid={data._id}>Accept As Co-Supervisor</Button>
                       
                     </CardActions>
                   </Card>
