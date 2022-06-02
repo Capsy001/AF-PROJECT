c010eb942ae13f25ec55c3fbd25d0dc883a3c55b
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2022-06-03 03:50:40 +0530

    pp

---
+++ frontend/src/components/appBarNav.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, AppBar, Toolbar, Divider, createTheme, colors } from "@mui/material";
+
+class AppBarNav extends Component {
+  constructor()
+  {
+    super();
+    this.isAdmin = false;
+  }
+
+  handleLogout = (event) => {
+    sessionStorage.setItem("logged", "false");
+
+    sessionStorage.setItem("loggedName", "NotLogged!");
+    sessionStorage.setItem("loggedEmail", "NotLogged!");
+    sessionStorage.setItem("loggedRole", "NotLogged!");
+
+    sessionStorage.clear;
+    window.location.href = "/";
+  };
+
+  componentWillMount() {
+    const logged = sessionStorage.getItem("logged");
+    const role = sessionStorage.getItem("loggedRole");
+
+    if(sessionStorage.getItem("loggedRole") == 'admin'){
+      this.isAdmin = true;
+    }else{
+      this.isAdmin = false;
+    }
+  }
+
+  render() {
+    return (
+      <AppBar position="static" color="success">
+        <Toolbar variant="dense">
+          <Link style={{ textDecoration: "none" }} to="/dashboard">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              Dashboard
+            </Button>
+          </Link>
+
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Link style={{ textDecoration: "none" }} to="/createSubmissionType">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              Create SubmissionTypes
+            </Button>
+          </Link>
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Link style={{ textDecoration: "none" }} to="/createStudentSubmission">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              Create Student SubmissionTypes
+            </Button>
+          </Link>
+          <Divider orientation="vertical" variant="middle" flexItem />
+
+          <Link style={{ textDecoration: "none" }} to="/createGroup">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              Create Group
+            </Button>
+          </Link>
+          <Divider orientation="vertical" variant="middle" flexItem />
+
+          <Link style={{ textDecoration: "none" }} to="/registerTopics">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              Register Topics
+            </Button>
+          </Link>
+          <Divider orientation="vertical" variant="middle" flexItem />
+          
+          <Link style={{ textDecoration: "none" }} to="/viewUsers">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              View Users
+            </Button>
+          </Link>
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Link style={{ textDecoration: "none" }} to="/viewStudentSubmissions">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              View Student SubmissionType
+            </Button>
+          </Link>
+          <Divider orientation="vertical" variant="middle" flexItem />
+
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <Link style={{ textDecoration: "none" }} to="/panelManagement">
+            <Button sx={{ color: "white" }} size="small" color="inherit">
+              Panel Member Managment
+            </Button>
+          </Link>
+          <Divider orientation="vertical" variant="middle" flexItem />
+
+          { this.isAdmin ? 
+            
+            <Link style={{ textDecoration: "none" }} to="/managePublications">
+              <Button sx={{ color: "white" }} size="small" color="inherit">
+                Publication
+              </Button>
+            </Link>
+
+            : 
+
+            <Link style={{ textDecoration: "none" }} to="/viewPublications">
+              <Button sx={{ color: "white" }} size="small" color="inherit">
+                Publication(s)
+              </Button>
+            </Link>
+
+            
+            
+          }
+
+        <Divider orientation="vertical" variant="middle" flexItem />
+
+        <Link style={{ textDecoration: "none" }} to="/supervisor">
+          <Button
+            sx={{ color: "white" }}
+            size="small"
+            color="inherit"
+            
+          >
+            Supervisor
+          </Button>
+        </Link>
+
+
+        <Divider orientation="vertical" variant="middle" flexItem />
+
+        <Link style={{ textDecoration: "none" }} to="/addMarkingSchema">
+          <Button
+            sx={{ color: "white" }}
+            size="small"
+            color="inherit"
+            
+          >
+            Marking Schema
+          </Button>
+        </Link>
+
+          <Divider orientation="vertical" variant="middle" flexItem />
+
+          <Link style={{ textDecoration: "none" }} to="/">
+            <Button
+              sx={{ color: "yellow" }}
+              size="small"
+              color="inherit"
+              onClick={this.handleLogout}
+            >
+              Logout
+            </Button>
+          </Link>
+
+         
+         
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default AppBarNav;
