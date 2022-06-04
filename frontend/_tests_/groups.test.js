8e5ecac95f4a3fadf8d00d5392006de6bab2de2c
Capsy <71144025+Capsy001@users.noreply.github.com>
2022-06-04 17:28:15 +0530

    c

--- frontend/_tests_/groups.test.js
+++ frontend/_tests_/groups.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { isTSAnyKeyword } from '@babel/types';
 
+import RegisterTopics from '../components/Student/RegisterTopics';
 
-test('use jsdom in this test file', () => {
+
+
+it('renders without error', () => {
     const element = document.createElement('div');
-    expect(element).not.toBeNull();
+    ReactDOM.render(<RegisterTopics></RegisterTopics>, element)
   });
